e80ba8c61291e776cfbd00db87b4035ff5d15875
Christian <chrisaug@rogers.com>
2021-04-24 13:27:16 -0400

    started ecomm v2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Izzy's Imperfect Items`,
+    description: `A green enterprise that takes pride in following a circular economy.`,
+    author: `Christian Augustyn <chrisaug@rogers.com>`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -29,6 +29,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-gatsby-cloud`,
+    `gatsby-plugin-postcss`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,8 +8,7 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
+import NavBar from './navbar/navbar'
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -25,23 +24,13 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div>
+        <NavBar/>
         <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
+        <footer>
+          © {new Date().getFullYear()}
           {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          Izzy's Imperfect Items
         </footer>
       </div>
     </>
