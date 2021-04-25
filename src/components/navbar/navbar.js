da0b1d2499a7a301bbeb5ece165d7abb5b6744cb
Christian <chrisaug@rogers.com>
2021-04-25 12:55:16 -0400

    addeed mobile view and desktop view

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react"
-import { Transition } from "@headlessui/react"
+import React, { useState, Fragment } from "react"
+import { Transition, Menu, Popover } from "@headlessui/react"
 import logo from "../../images/logo-b.png"
 
 const NavBar = () => {
@@ -7,10 +7,26 @@ const NavBar = () => {
 
   return (
     <div>
-      <div className="container flex md:hidden flex-row-reverse">
+      <div className="container flex md:hidden flex-row justify-between">
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+            />
+          </svg>
+        </button>
         {/* menu buttons */}
         {!isOpen ? (
-          <button className="z-50" onClick={() => setIsOpen(!isOpen)}>
+          <button onClick={() => setIsOpen(!isOpen)}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6"
@@ -27,7 +43,7 @@ const NavBar = () => {
             </svg>
           </button>
         ) : (
-          <button className="z-50" onClick={() => setIsOpen(!isOpen)}>
+          <button onClick={() => setIsOpen(!isOpen)}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6"
@@ -54,28 +70,115 @@ const NavBar = () => {
         <div className="container mx-auto">
           <ul className="hidden md:flex flex-row justify-center">
             <li className="pr-4">Home</li>
-            <li className="pr-4">Products</li>
+            {/* Drop down menu for the products */}
+            <li className="pr-4">
+              <Popover className="relative">
+                {({ open }) => (
+                  <>
+                    <Popover.Button>Products</Popover.Button>
+
+                    <Transition
+                      show={open}
+                      as={Fragment}
+                      enter="transition ease-out duration-200"
+                      enterFrom="opacity-0 translate-y-1"
+                      enterTo="opacity-100 translate-y-0"
+                      leave="transition ease-in duration-150"
+                      leaveFrom="opacity-100 translate-y-0"
+                      leaveTo="opacity-0 translate-y-1"
+                    >
+                      <Popover.Panel className="absolute z-10 w-52">
+                        <div className="grid grid-cols-1 border-solid rounded shadow-sm p-3 pt-0 mt-3 bg-purple-50">
+                          <a href="#" className="my-3">
+                            Scrunchies
+                          </a>
+                          <a href="#" className="my-3">
+                            Specialty Scrunchies
+                          </a>
+                          <a href="#" className="my-3">
+                            Cutlery Pouches
+                          </a>
+                          <a href="#" className="my-3">
+                            Napkins
+                          </a>
+                          <a href="#" className="my-3">
+                            Face Masks
+                          </a>
+                          <a href="#" className="my-3">
+                            Bowl Cozies
+                          </a>
+                        </div>
+                      </Popover.Panel>
+                    </Transition>
+                  </>
+                )}
+              </Popover>
+            </li>
             <li className="pr-4">About</li>
             <li className="pr-4">Contact</li>
             <li className="pr-4">Questions</li>
-            <li className="pr-4">Cart</li>
+            <li className="pr-4 flex flex-row">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+            </li>
           </ul>
         </div>
       </nav>
       {/* side nav on pop up */}
-        <Transition
-			show={isOpen}
-			enter="transition-opacity duration-75"
-			enterFrom="opacity-0"
-			enterTo="opacity-100"
-			leave="transition-opacity duration-150"
-			leaveFrom="opacity-100"
-			leaveTo="opacity-0"
-		>
-          <div className="z-20 absolute inset-0">
-            <div className="float-right w-64 h-screen bg-purple-300">swag</div>
+      <Transition
+        show={isOpen}
+        enter="ease-in-out duration-300"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="ease-in-out duration-300"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div className="md:invisible z-20 absolute inset-0 flex flex-row-reverse item-start">
+          {/* Side nav, Navigation */}
+          <div className="z-40 w-64 h-full bg-white">
+            <ul className="my-4">
+              <li className="pr-4">Home</li>
+              <li className="pr-4">Products</li>
+              <li className="pr-4">About</li>
+              <li className="pr-4">Contact</li>
+              <li className="pr-4">Questions</li>
+            </ul>
           </div>
-        </Transition>
+          <button className="z-40 h-3" onClick={() => setIsOpen(false)}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="white"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+          <div
+            className="absolute inset-0 bg-black bg-opacity-50"
+            onClick={() => setIsOpen(false)}
+          />
+        </div>
+      </Transition>
     </div>
   )
 }
